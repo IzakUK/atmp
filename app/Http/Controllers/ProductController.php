257d400f7ff76cb3fa8c0106a20660f6f7ffc67b
MrMonixIsMonil <Monil2K16@outlook.com>
2024-12-02 22:23:25 +0000

    Completed basic function of stock instake

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,9 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');
+        $products = Product::all();
+        return view('products.index', ['products' => $products]);
+    
     }
 
     public function create(){
@@ -27,5 +29,26 @@ class ProductController extends Controller
 
         return redirect(route('product.index'));
 
+    }
+
+    public function edit(Product $product){
+        return view('products.edit', ['product' => $product]);
+    }
+    public function update(Product $product, Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable'
+        ]);
+
+        $product->update($data);
+        return redirect(route('product.index'))->with('success', 'Product Updated Successfully');
+
+    }
+
+    public function destroy(Product $product){
+        $product->delete();
+        return redirect(route('product.index'))->with('success', 'Product  Deleted Successfully');
     }
 }
